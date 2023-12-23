0a5679fadddbdd8987f3d45e4fe8278cefb165e2
vikaazh <114040365+vikaazh@users.noreply.github.com>
2023-12-23 10:06:17 +0700

    Update test_func.py

--- test_func.py
+++ test_func.py
@@ -9,7 +9,7 @@ class TestTriangleType(unittest.TestCase):
         self.assertEqual(get_triangle_type(10, 10, 10), 'equilateral')
 
     def test_nonequilateral(self):
-        self.assertEqual(get_triangle_type(1, 2, 3), 'nonequilateral')
+        self.assertEqual(get_triangle_type(2, 3, 4), 'nonequilateral')
 
     def test_isosceles(self):
         self.assertEqual(get_triangle_type(2, 2, 1), 'isosceles')
@@ -17,10 +17,7 @@ class TestTriangleType(unittest.TestCase):
         self.assertEqual(get_triangle_type(1, 2, 2), 'isosceles')
 
     def test_incorrect_sides(self):
-        self.assertEqual(get_triangle_type(2, 2, 4), 'IncorrectTriangleSides')
-        self.assertEqual(get_triangle_type(0, 0, 0), 'IncorrectTriangleSides')
-        self.assertEqual(get_triangle_type(6, 8, 10), 'IncorrectTriangleSides')
-
+        self.assertEqual(get_triangle_type(1, 1, 10), 'IncorrectTriangleSides')
 
 if __name__ == '__main__':
     unittest.main()
